4ca4f1d4b5735888012b2d6de5a972611d318ff6
Simone Schiavo <simoneschiavo@icloud.com>
2024-05-31 16:20:27 +0200

    Update Gameboard.render() to use better syntax

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const Gameboard = (function () {
     const board = ["", "", "", "", "", "", "", "", ""];
 
     const gameboardDiv = document.querySelector("#gameboard");
+
     const render = () => {
         gameboardDiv.innerHTML = "";
         board.forEach((cell, position) => {
-            gameboardDiv.innerHTML += `<div class="cell" id="cell-${position}">${cell}</div>`;
+            const cellDiv = document.createElement("div");
+            cellDiv.classList.add("cell");
+            cellDiv.setAttribute("data-attribute", `cell-${position}`);
+            const cellToken = document.createElement("div");
+            cellToken.classList.add("token");
+            cellToken.textContent = cell;
+            cellDiv.appendChild(cellToken);
+            gameboardDiv.appendChild(cellDiv);
         });
     };
 
