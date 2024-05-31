8ea02075a7b0a09ae3e5af0bba5da26ca1a2a8ec
Simone Schiavo <simoneschiavo@icloud.com>
2024-05-31 14:13:08 +0200

    Change getBoard with render function
    I've also simplified the board array from a 2d arr
    to a 1d arr with 9 empty elements

--- script.js
+++ script.js
@@ -2,19 +2,11 @@
 const Gameboard = (function () {
     const rows = 3;
     const columns = 3;
-    const board = [];
+    const board = ["", "", "", "", "", "", "", "", ""];
 
-    // Create a 2D array - the console gameboard
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push("x");
-        };
-    };
+    const render = () => board;
 
-    const getBoard = () => board;
-
-    return { getBoard };
+    return { render };
 })();
 
 // console.table(Gameboard.getBoard());
